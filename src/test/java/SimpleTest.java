795bf1b53063becb98fca501c38464fbe30dde8b
Artemiy Skryabin <artemio.skr@gmail.com>
2021-02-18 01:17:10 +0700

    WIP

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,4 +1,4 @@
-import io.qameta.allure.Description;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -7,13 +7,13 @@ import static org.hamcrest.Matchers.*;
 public class SimpleTest {
 
     @Test
-    @Description("5+5 = 10")
+    @DisplayName("5+5 = 10")
     void PassedTest() {
         assertThat(5 + 5, is(equalTo(10)));
     }
 
     @Test
-    @Description("5+5 != 10")
+    @DisplayName("5+5 != 10")
     void FailedTest() {
         assertThat(5 + 5, is(not(10)));
     }
